71fe77ef2a0e1c48a73472e9d40dedf9b42adb10
Mohamed-Apo-Hipa <mohamedadel31038@gmail.com>
2020-01-02 00:27:19 +0200

    show page

--- src/js/script.js
+++ src/js/script.js
@@ -1,4 +1,15 @@
 $(document).ready(function () {
 
+   $(window).on('scroll', function () {
+
+        if($(this).scrollTop() > 100)
+        {
+            $('.navbar').addClass('bg-black-transparent')
+        }else{
+            
+            $('.navbar').removeClass('bg-black-transparent')
+        }
+
+   }) 
 
 })
